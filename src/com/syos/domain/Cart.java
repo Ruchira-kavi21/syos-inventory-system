ba186b403990629c395636a8e7c154c553ed8e81
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2025-12-30 11:18:57 +0530

    Added cart entity to handle temporary items

---
+++ src/com/syos/domain/Cart.java
@@ -0,0 +1,30 @@
+package com.syos.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private final Map<Item, Integer> items = new HashMap<>();
+
+    public void addItem(Item item, int quantity) {
+        items.put(item, items.getOrDefault(item, 0) + quantity);
+    }
+    public Map<Item, Integer> getItems(){
+        return items;
+    }
+    public double calculateTotal(){
+        double total = 0;
+        for (Map.Entry <Item, Integer> entry : items.entrySet()){
+            Item item = entry.getKey();
+            int quantity = entry.getValue();
+            total += item.getPrice() * quantity;
+        }
+        return total;
+    }
+    public void clearCart(){
+        items.clear();
+    }
+    public boolean isEmpty(){
+        return items.isEmpty();
+    }
+}
