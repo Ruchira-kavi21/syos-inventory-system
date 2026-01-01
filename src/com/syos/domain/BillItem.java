681757bd0b85d5d61fdf47ebc52ccabe9623264c
Ruchira Kaveesha <ruchirakaveesha2020@gmail.com>
2026-01-01 13:03:05 +0530

    add unit price method and minor changes

--- src/com/syos/domain/BillItem.java
+++ src/com/syos/domain/BillItem.java
@@ -1,22 +1,27 @@
 package com.syos.domain;
 
 public class BillItem {
-    private final Item itemName;
+    private final String itemName;
     private final int quantity;
+    private final double unitPrice;
     private final double totalPrice;
 
-    public BillItem(Item itemName, int quantity, double totalPrice) {
+    public BillItem(String itemName, int quantity, double unitPrice, double totalPrice) {
         this.itemName = itemName;
         this.quantity = quantity;
+        this.unitPrice = unitPrice;
         this.totalPrice = totalPrice;
     }
 
-    public Item getItem() {
+    public String getItemName() {
         return itemName;
     }
 
     public int getQuantity() {
         return quantity;
+    }
+    public double getUnitPrice() {
+        return unitPrice;
     }
 
     public double getTotalPrice() {
